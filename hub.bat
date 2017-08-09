8c3a9bd10f4ed78644a1924eecffff94eebae868
LytenkoMihail <30859489+LytenkoMihail@users.noreply.github.com>
2017-08-09 12:53:07 +0300

    testing

--- hub.bat
+++
@@ -1,6 +0,0 @@
-echo "# SF20171" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/LytenkoMihail/SF20171.git
-git push -u origin master
