99c054e08cd36ae020b6a2afb9315151cbfdf354
LytenkoMihail <30859489+LytenkoMihail@users.noreply.github.com>
2017-08-25 16:10:34 +0300

    Хранение настроек в property-файлах

--- src/ua/amper/kharkov/sf/util/LoadPropertyFromFile.java
+++ src/ua/amper/kharkov/sf/util/LoadPropertyFromFile.java
@@ -1,9 +1,12 @@
 package ua.amper.kharkov.sf.util;
 
+import com.sun.deploy.util.SyncFileAccess;
+
 import javax.swing.*;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 import static ua.amper.kharkov.sf.SFConstants.LOGGER_ERROR_NOT_OPEN_FILE;
@@ -22,23 +25,23 @@ public class LoadPropertyFromFile {
     private Properties properties;
     private FileInputStream fileInputStream;
 
-    public LoadPropertyFromFile(String filePropetries) {
+    public LoadPropertyFromFile(String fileNamePropetries) {
         setPropertyFile(false);
         Properties properties = new Properties();
         try {
             //обращаемся к файлу и получаем данные
-//            fileInputStream = new FileInputStream(getClass().getClassLoader().getResource(filePropetries).getPath());
-            fileInputStream = new FileInputStream(getClass().getClassLoader().getResource(filePropetries).getFile());
-            String s = getClass().getClassLoader().getResource(filePropetries).getFile();
+            InputStream fileInputStream = getClass().getClassLoader().getResourceAsStream(fileNamePropetries);
+            if (fileInputStream != null) {
+                properties.load(fileInputStream);
+                System.out.println(fileInputStream.toString()+"=========="+properties.getProperty("USER_NAME"));
+            }
 
-//            fileInputStream = new FileInputStream(filePropetries);
-            properties.load(fileInputStream);
-            System.out.println(s+"=========="+properties.getProperty("USER_NAME"));
+
         } catch (FileNotFoundException e) {
-            System.out.println(LOGGER_ERROR_NOT_OPEN_FILE + ":" + filePropetries);
+            System.out.println(LOGGER_ERROR_NOT_OPEN_FILE + ":" + fileNamePropetries);
             e.printStackTrace();
         } catch (IOException e) {
-            System.out.println(LOGGER_ERROR_NOT_OPEN_FILE + ":" + filePropetries);
+            System.out.println(LOGGER_ERROR_NOT_OPEN_FILE + ":" + fileNamePropetries);
             e.printStackTrace();
         }
     }
