34dfc111828ba6f40bd2a177a8b6fcf9242d1000
LytenkoMihail <30859489+LytenkoMihail@users.noreply.github.com>
2018-03-26 15:32:33 +0300

    Развитие программы Счет фактура.

--- src/ua/amper/kharkov/sf/SFConstants.java
+++ src/ua/amper/kharkov/sf/SFConstants.java
@@ -6,13 +6,13 @@ package ua.amper.kharkov.sf;
 public final class SFConstants {
     public static final String SF_VERSION = "Счет-фактура версия 2017.1.0";
 
-    public static final String LOGGER_START                 = "START >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>";
-    public static final String LOGGER_END                   = "STOP  <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<";
+    public static final String LOGGER_START = "START >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>";
+    public static final String LOGGER_END = "STOP  <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<";
 
     public static final String LOGGER_GETUSERPASSWORDWINDOW = "";
-    public static final String LOGGER_MAINWINDOW            = "";
-    public static final String LOGGER_LOADIMAGESFROMFILE           = "";
-    public static final String LOGGER_LOADPROPERTYFROMFILE           = "";
+    public static final String LOGGER_MAINWINDOW = "";
+    public static final String LOGGER_LOADIMAGESFROMFILE = "";
+    public static final String LOGGER_LOADPROPERTYFROMFILE = "";
 
 
     public static final String LOGGER_ERROR_NOT_OPEN_FILE = "NOT OPEN FILE:";
@@ -26,13 +26,13 @@ public final class SFConstants {
 
     public static final String MSG_USER_NAME = "Пользователь:";
     public static final String MSG_USER_PASSWORD = "Пароль:      ";
-    public static final String MSG_USER_NAME_PASSWORD_ERROR = "Идентификая пользователя не выполнена.\n"+"Неправильное имя и пароль пользователя.";
+    public static final String MSG_USER_NAME_PASSWORD_ERROR = "Идентификая пользователя не выполнена.\n" + "Неправильное имя и пароль пользователя.";
 
     public static final String MSG_DB_MY_SQL = "MY SQL";
-    public static final String MSG_DB_CONNECTINGDATABASE_ERROR = "База данных "+MSG_DB_MY_SQL+" не обнаружена.";
-    public static final String MSG_DB_DISCONNECTINGDATABASE_ERROR = "База данных"+MSG_DB_MY_SQL+" не отключена.";
+    public static final String MY_SQL_TABLE_NAME_USER = "<user>";
 
-
+    public static final String MSG_DB_CONNECTINGDATABASE_ERROR = "СУБД " + MSG_DB_MY_SQL + " не обнаружена.";
+    public static final String MSG_DB_DISCONNECTINGDATABASE_ERROR = "СУБД" + MSG_DB_MY_SQL + " не отключена.";
 
 
     public static final String SF_RESOURCES_FILE_ICON_PROGRAMM = "Images/invoice_document_file.png";
@@ -44,10 +44,10 @@ public final class SFConstants {
 
     public static final String SF_SQL_FILE_NOT_OPEN = "Ошибка открытия файла с SQL запросом!";
     public static final String SF_SQL_EXEC_SQL = "Ошибка выполнения SQL запроса!";
-    public static final String SF_SQL_EXEC_NOT_RECORD_TABLE = "В таблице нет записей!";
+    public static final String SF_SQL_EXEC_NOT_RECORD_TABLE = "В таблице " + MY_SQL_TABLE_NAME_USER + " нет записей!";
 
 
-    public static final String ECHO_CHAR_USER_PASSWORD= "*";
+    public static final String ECHO_CHAR_USER_PASSWORD = "*";
 
     public static final String MY_SQL_LOCAL_HOST = "localhost";
     public static final String MY_SQL_DATA_BASE = "sfbase";
